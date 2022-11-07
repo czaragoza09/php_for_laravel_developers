871884d14dc351c402eb503d6f8e3cbb31a8af5a
czaragoza09 <czaragoza@iesebre.com>
2022-11-07 16:07:44 +0100

    index.php del app modificat

--- app/index.php
+++ app/index.php
@@ -1,3 +1,20 @@
 <?php
 
-$greeting = 'Hola món!';
+//QUERY STRING
+//var_dump ($_GET['name']);
+
+//$name = 'Cinta';
+//API
+function great() {
+    $name = htmlspecialchars($_GET['name']);
+    $surname = $_GET['surname'];
+
+    return "Hola $name  $surname !";
+}
+$greeting = great();
+//echo great();
+
+
+
+//$greeting = 'Hola' . $_GET['name'] . ' '.  $_GET['surname'] . '!';
+

2b067850e6e82f339c6002f97d1c59396d569018
czaragoza09 <czaragoza@iesebre.com>
2022-12-05 21:06:30 +0100

    Versió 0.7

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 
 return [
     'database' => [
-        'user' => 'debian-sys-maint',
-        'password' => '2TgF0Sl0Meh3le4E',
-        'databasetype' => 'mysql',
-        'host' => 'localhost',
-        'name' => 'phplaraveldevs',
+        'user' => $_ENV['DB_USERNAME'],
+        'password' => $_ENV['DB_PASSWORD'],
+        'databasetype' => $_ENV['DB_CONNECTION'],
+        'host' => $_ENV['DB_HOST'],
+        'name' => $_ENV['DB_DATABASE'],
     ]
 ];
